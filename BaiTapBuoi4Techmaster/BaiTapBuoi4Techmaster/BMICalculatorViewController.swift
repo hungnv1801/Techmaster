d012a81bceae79dce7810e330943187e3f53039c
hung <hungnv1801@gmail.com>
2020-10-30 15:50:00 +0700

    Baitapbuoi4

---
+++ BaiTapBuoi4Techmaster/BaiTapBuoi4Techmaster/BMICalculatorViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BMICalculatorViewController.swift
+//  BaiTapBuoi4Techmaster
+//
+//  Created by Nguyen Trung on 10/29/20.
+//  Copyright © 2020 Nguyen Hung. All rights reserved.
+//
+
+import UIKit
+
+class BMICalculatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+}
---
+++ BaiTapBuoi4Techmaster/BaiTapBuoi4Techmaster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BaiTapBuoi4Techmaster
+//
+//  Created by Nguyen Trung on 10/29/20.
+//  Copyright © 2020 Nguyen Hung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
